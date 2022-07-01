298ff490777ca766a36ed4881aee58472c8538bb
Apurv <apurv.nnd@gmail.com>
2022-07-01 23:16:04 +0530

    Sort Unique Elements at start

---
+++ LeetCode/RemoveDuplicatesFromSortedArray.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        nums_count = Counter(nums)
+        unique_nums = list(nums_count.keys())
+        for i in range(len(unique_nums)):
+            nums[i]=unique_nums[i]
+        return len(unique_nums)
