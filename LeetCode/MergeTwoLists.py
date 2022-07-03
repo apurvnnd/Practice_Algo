b7ad47247e93af56c152c75e53c9c5323951c4dd
Apurv <apurv.nnd@gmail.com>
2022-07-03 16:19:24 +0530

    merge list changes

--- LeetCode/MergeTwoLists.py
+++ LeetCode/MergeTwoLists.py
@@ -1,20 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        head=ListNode()
-        t=head
-        while(l1 and l2):
-            if l1.val < l2.val:
-                t.next = ListNode(l1.val)
-                l1 = l1.next
-            elif l1.val >= l2.val:
-                t.next = ListNode(l2.val)
-                l2 = l2.next
-            t = t.next
-        
-
-        if l1 is not None:
-            t.next=l1
-        if l2 is not None:
-            t.next=l2
-            
-        return head.next
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        final = ListNode()
+        final_node = final
+        while list1 and list2:
+            if list1.val > list2.val:
+                final_node.next = ListNode(list2.val)
+                list2 = list2.next
+            else:
+                final_node.next = ListNode(list1.val)
+                list1 = list1.next
+            final_node = final_node.next
+        if list1:
+            final_node.next = list1
+        elif list2:
+            final_node.next = list2
+        return final.next
